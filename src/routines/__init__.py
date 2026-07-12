f8dcfd7cb9c62df38854c41b1cd68cef2c980407
Aditi370 <aditis11@illinois.edu>
2026-07-12 18:11:23 -0500

    Add FORMS orbit notebook and routines

---
+++ src/routines/__init__.py
@@ -0,0 +1,8 @@
+from .sample_dipole_magnetic_field import SampleDipoleMagneticFieldRoutine
+from .sample_igrf13_magnetic_field import SampleIGRF13MagneticFieldRoutine
+
+
+__all__ = [
+    "SampleDipoleMagneticFieldRoutine",
+    "SampleIGRF13MagneticFieldRoutine",
+]
